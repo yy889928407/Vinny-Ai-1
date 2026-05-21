ec97f0d03e5fceb9221986d3c68c6f9df8376232
Your Name <your@email.com>
2026-05-21 21:17:59 +0300

    Fix GitHub Pages structure

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", backend_status="Running")
+
+@app.route("/status")
+def status():
+    return jsonify(status="running")
 
 if __name__ == "__main__":
     app.run(debug=True)
